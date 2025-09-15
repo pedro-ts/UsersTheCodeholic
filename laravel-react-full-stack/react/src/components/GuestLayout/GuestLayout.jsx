6b21e5ce1eec856b01340d35065d79fa06585365
pedrots <pedrotorres9843@gmail.com>
2025-09-15 06:52:00 -0300

    Criação de layouts, aplicação de filhos em rotas, exbição dos filhos nos layouts(Outlet) e ajuste para não dar notFound na rota /

---
+++ laravel-react-full-stack/react/src/components/GuestLayout/GuestLayout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+
+
+const GuestLayout = () => {
+  return (
+    <div>
+      GuestLayout
+      <Outlet/>
+    </div>
+  )
+}
+
+export default GuestLayout
--- laravel-react-full-stack/react/src/router.jsx
+++ laravel-react-full-stack/react/src/router.jsx
@@ -1,28 +1,54 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 // Pages
-import Login from "./views/Login/Login"
-import Signup from "./views/Signup/Signup"
-import Users from "./views/Users/Users"
-import NotFound from "./views/NotFound/NotFound"
+import Login from "./views/Login/Login";
+import Signup from "./views/Signup/Signup";
+import Users from "./views/Users/Users";
+import Dashboard from "./views/Dashboard/Dashboard";
+import NotFound from "./views/NotFound/NotFound";
+// Components
+import GuestLayout from "./components/GuestLayout/GuestLayout";
+import DefaultLayout from "./components/DefaultLayout/DefaultLayout";
 
 const router = createBrowserRouter([
-{
-    path: '/login',
-    element: <Login/>
-},
-{
-    path: '/signup',
-    element: <Signup/>
-},
-{
-    path: '/users',
-    element: <Users/>
-},
-{
-    path: '*',
-    element: <NotFound/>
-}
-
-])
+  // Layout padrão
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/users" />
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/users",
+        element: <Users/>
+      }
+    ],
+  },
+  // Layout de login ou registro
+  {
+    path: "/",
+    element: <GuestLayout />,
+    children: [
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+  // NotFound
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
 
 export default router;
