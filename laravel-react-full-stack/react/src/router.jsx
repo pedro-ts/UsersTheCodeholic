ea9357a073af6a01b897d222575a55b588049bc6
pedrots <pedrotorres9843@gmail.com>
2025-09-15 06:34:13 -0300

    Ajuste na localização da pasta views

--- laravel-react-full-stack/react/src/router.jsx
+++ laravel-react-full-stack/react/src/router.jsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 // Pages
-import Login from "./assets/views/Login/Login"
-import Signup from "./assets/views/Signup/Signup"
-import Users from "./assets/views/Users/Users"
-import NotFound from "./assets/views/NotFound/NotFound"
+import Login from "./views/Login/Login"
+import Signup from "./views/Signup/Signup"
+import Users from "./views/Users/Users"
+import NotFound from "./views/NotFound/NotFound"
 
 const router = createBrowserRouter([
 {
