d406883e17d57d61f8f9838d5407944d74b9837f
pedrots <pedrotorres9843@gmail.com>
2025-09-15 06:32:34 -0300

    Criação de rotas e aplicação no main e view + rota notFound

--- laravel-react-full-stack/react/src/router.jsx
+++ laravel-react-full-stack/react/src/router.jsx
@@ -1,6 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
+// Pages
+import Login from "./assets/views/Login/Login"
+import Signup from "./assets/views/Signup/Signup"
+import Users from "./assets/views/Users/Users"
+import NotFound from "./assets/views/NotFound/NotFound"
 
 const router = createBrowserRouter([
+{
+    path: '/login',
+    element: <Login/>
+},
+{
+    path: '/signup',
+    element: <Signup/>
+},
+{
+    path: '/users',
+    element: <Users/>
+},
+{
+    path: '*',
+    element: <NotFound/>
+}
 
 ])
 
