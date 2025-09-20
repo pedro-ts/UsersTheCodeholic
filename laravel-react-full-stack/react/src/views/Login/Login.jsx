19463ac3ca9b01ca7fb5a9d396afde1f5fc5a43d
pedrots <pedrotorres9843@gmail.com>
2025-09-20 10:11:36 -0300

    Implementação da logica no react do login(caso queira corrigir o erro de não exibir a mensagem de 'credencias invalidas' só adicionar status code no laravel)

--- laravel-react-full-stack/react/src/views/Login/Login.jsx
+++ laravel-react-full-stack/react/src/views/Login/Login.jsx
@@ -1,24 +1,70 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
+import axiosClient from "../../axios-client";
+import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { useStateContext } from "../../context/ContextProvider";
+
 const Login = () => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
 
-  const onSubmit = (e) =>{
+  const [errors, setErrors] = useState(null);
+  const [message, setMessage] = useState(null);
+  // Importação dos set's do contexto
+  const { setUser, setToken } = useStateContext();
+
+  const onSubmit = (e) => {
     e.preventDefault();
+    setErrors(null);
+    setMessage(null);
 
+    const payload = {
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+    };
+    // Post para o backend usando o axiosClient criado em "../../axiosClient.js"
+    axiosClient
+      .post("/login", payload)
+      .then(({ data }) => {
+        setUser(data.user);
+        setToken(data.token);
+      })
+      .catch((error) => {
+        const response = error.response;
+        if (response && response.status == 422) {
+          setErrors(response.data.errors);
+        } else{
+          setMessage(response.data.message);
+        }
+      });
   };
 
   return (
-        <form onSubmit={onSubmit}>
-          <h1 className='title'>Login into your account</h1>
-          <input type="email" placeholder='Email' id="" />
-          <input type="password" placeholder='Password' id="" />
+    <form onSubmit={onSubmit}>
+      <h1 className="title">Login into your account</h1>
+      {message && (
+        <div className="alert">
+          <p>{message}</p>
+        </div>
+      )}
 
-          <button className='btn btn-block'>Login</button>
-          <p className="message">
-            Not registered? <Link to="/signup">Create an account</Link>
-          </p>
-        </form>
-  )
-}
+      {errors && (
+        <div className="alert">
+          {Object.keys(errors).map((key) => (
+            <p key={key}>{errors[key][0]}</p>
+          ))}
+        </div>
+      )}
 
-export default Login
+
+      <input ref={emailRef} type="email" placeholder="Email" />
+      <input ref={passwordRef} type="password" placeholder="Password" />
+
+      <button className="btn btn-block">Login</button>
+      <p className="message">
+        Not registered? <Link to="/signup">Create an account</Link>
+      </p>
+    </form>
+  );
+};
+
+export default Login;
